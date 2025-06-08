0c2324a3a1c7f4c73d4a4a05ca59eb24daba4b0f
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-06-08 17:00:45 +0100

    add logging config

--- src/leaderboard/settings.py
+++ src/leaderboard/settings.py
@@ -136,18 +136,33 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
+    'formatters': {
+        'detailed': {
+            'format': '[{levelname}] {asctime} {name} {message}',
+            'style': '{',
         },
     },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'detailed',
+            'level': 'DEBUG',
+        },
+    },    
     'loggers': {
         'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': False,
         },
+        'upload': {  # Explicitly allow logging for views in the upload app
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+    'root': {  # Ensures general loggers work
+        'handlers': ['console'],
+        'level': 'DEBUG',
     },
 }
