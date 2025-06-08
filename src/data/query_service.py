8c03188865c147bf60eda0c0144b2b491847ffa7
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-06-08 22:52:25 +0100

    plumb in the rankings data into a rudimentary table

--- src/data/query_service.py
+++ src/data/query_service.py
@@ -1,5 +1,4 @@
-from .models import Contestant, Submission, SubmissionDto
-from django.db.models import Count
+from .models import Contestant, Ranking, RankingSubmission, RankingDto, Submission, SubmissionDto
 
 class QueryService:
     def fetch_submissions_for_contestant(self, contestant_name: str,
@@ -8,3 +7,18 @@ class QueryService:
         submissions = Submission.objects.filter(contestant=contestant).order_by('date')[:limit]
         return [SubmissionDto(s.contestant.name, s.competition.name, s.date, s.score) for s in submissions]
 
+    def fetch_rankings(self) -> list[RankingDto]:
+        ranking_data: list[RankingDto] = []
+        rankings = Ranking.objects.all().order_by('-total_score', '-num_submissions_included')
+        
+        for ranking in rankings:
+            submissions = [
+                SubmissionDto(s.submission.contestant.name, s.submission.competition.name, s.submission.date, s.submission.score)
+                for s in RankingSubmission.objects.filter(ranking=ranking)
+            ]
+            ranking_data.append(RankingDto(contestant_name=ranking.contestant.name,
+                                           total_score=ranking.total_score,
+                                           latest_submission_date=ranking.latest_submission_date,
+                                           num_submissions_included=ranking.num_submissions_included,
+                                           submissions=submissions))
+        return ranking_data
--- src/display/views.py
+++ src/display/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from data.query_service import QueryService
 
 # Create your views here.
-def display_scores(request,
-                   query_service: QueryService = QueryService()):
-    relevant_contestants = query_service.fetch_contestants()
-    return HttpResponse(f"Found {len(relevant_contestants)} users with at least 3 entries")
+def display_scores(request, query_service: QueryService = QueryService()):
+    rankings = query_service.fetch_rankings()
+    context = {'rankings': rankings}
+    return render(request, 'rankings_table.html', context)
