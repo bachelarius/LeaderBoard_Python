b7779e392b274ab4fcaf91ac9358f08ad5c1a85c
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-06-09 01:49:01 +0100

    add styling to upload page - added link to upload page if there is no data uploaded

--- src/upload/views.py
+++ src/upload/views.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import json
 import logging
@@ -29,7 +29,7 @@ def upload_json(request, upload_service: DataParserService = DataParserService()
                 if (ranking):
                     command_service.upsert_ranking(ranking)
 
-            return HttpResponse(f"Uploaded {len(parsed.contestants())} contestants with {len(parsed.submissions())} submissions across {len(parsed.competitions())} competitions.")
+            return redirect('display_scores')
         
         except json.JSONDecodeError as e:
             logger.error(f"Error during JSON upload: {e}", exc_info=True)
