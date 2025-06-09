71f239e87dd7027f3d668a9b39fee8a4f7b52d6e
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-06-09 02:51:36 +0100

    test fix

--- src/upload/tests.py
+++ src/upload/tests.py
@@ -11,7 +11,7 @@ class RankingGeneratorServiceTest(TestCase):
         query_service_mock = MagicMock()
         submission1 = SubmissionDto(contestant_name="test_contestant", competition_name="test_competition_1", date=date(2023, 1, 1), score=10)
         submission2 = SubmissionDto(contestant_name="test_contestant", competition_name="test_competition_2", date=date(2022, 1, 2), score=20)
-        submission3 = SubmissionDto(contestant_name="test_contestant", competition_name="test_competition_3", date=date(2021, 1, 2), score=30)
+        submission3 = SubmissionDto(contestant_name="test_contestant", competition_name="test_competition_3", date=date(2021, 1, 3), score=30)
         query_service_mock.fetch_submissions_for_contestant.return_value = [submission1, submission2, submission3]
 
         ranking_generator_service = RankingGeneratorService(config=config, query_service=query_service_mock)
@@ -21,7 +21,7 @@ class RankingGeneratorServiceTest(TestCase):
         if ranking:
             self.assertEqual(ranking.contestant_name, "test_contestant")
             self.assertEqual(ranking.total_score, 60)
-            self.assertEqual(ranking.latest_submission_date, date(2023, 1, 2))
+            self.assertEqual(ranking.latest_submission_date, date(2023, 1, 1))
             self.assertEqual(ranking.num_submissions_included, 3)
 
     def test_not_generate_ranking_not_enough_submissions(self):
